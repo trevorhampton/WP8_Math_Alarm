7a6f677cd7cef644292931462cf6481601d6c84b
Trevor <trevorhampton@outlook.com>
2014-07-30 23:19:36 -0400

    Second commit.  Begins math problem logic.
    This commit adds the basicMathProblem method that starts to create and solve the math problem.  Currnently doesn't do anything besides create an easy problem and solves it.  No method of output yet.

--- Math_Alarm/Math_Alarm/AlarmCreate.xaml.cs
+++ Math_Alarm/Math_Alarm/AlarmCreate.xaml.cs
@@ -8,11 +8,13 @@ using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
 using Microsoft.Phone.Scheduler;
+using xFunc.Maths;
 
 namespace Math_Alarm
 {
     public partial class AlarmCreate : PhoneApplicationPage
     {
+        String difficutlyLvl; 
         public AlarmCreate()
         {
             InitializeComponent();
@@ -35,5 +37,64 @@ namespace Math_Alarm
             //and sets it to be an actual scheduled service by the OS.
             ScheduledActionService.Add(alarm);
         }
+
+        //Method that decides and calculates the math problem that turns
+        //off the alarm.  This method might be moved to another class e.g. MainPage.xaml.
+        private void basicMathProblem()
+        {
+            Random random = new Random();
+            int operationNum = random.Next(0, 1), answerNum;
+            String operation;
+
+            switch (operationNum)
+            {
+                case 0:
+                    operation = "-";
+                    break;
+                case 1:
+                    operation = "*";
+                    break;
+                default:
+                    operation = "+";
+                    break;
+            }
+            
+            if (difficutlyLvl.Equals("Easy"))
+            {
+                if (operation.Equals("+"))
+                {
+                    answerNum = random.Next(0, 100) + random.Next(0, 100);
+                }
+
+                else if (operation.Equals("-"))
+                {
+                    answerNum = random.Next(0, 100) - random.Next(0, 100);
+                }
+
+                else
+                {
+                    answerNum = random.Next(0, 100) * random.Next(0, 100);
+                }
+            }
+
+            else if (difficutlyLvl.Equals("Normal"))
+            {
+               
+            }
+
+            else
+            {
+                
+            }
+        }
+        
+        //This method will return the level of difficulty that user selects.
+        //private String getDifficulty()
+        //{
+        //    //This line will be assign by the value of the difficultyBox object
+        //    //to the difficultyLvl string once the user makes a selection from it. 
+
+        //    return;
+        //}
     }
 }
