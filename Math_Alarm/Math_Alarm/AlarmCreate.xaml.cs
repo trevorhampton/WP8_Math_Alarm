7d7142cf6e2634737b2aa2a4a8a1a99f6d9c6195
Trevor <trevorhampton@outlook.com>
2014-08-09 16:21:38 -0400

    Fixed AlarmCreate scrolling issue.
    Adds a blank text block to the bottom of the AlarmCreate page in order to make the page scroll far enough to reveal all of the items in the list picker.  Temporary fix, and should be better implemented later.

--- Math_Alarm/Math_Alarm/AlarmCreate.xaml.cs
+++ Math_Alarm/Math_Alarm/AlarmCreate.xaml.cs
@@ -8,12 +8,13 @@ using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
 using Microsoft.Phone.Scheduler;
+using Microsoft.Phone.Notification;
 
 namespace Math_Alarm
 {
     public partial class AlarmCreate : PhoneApplicationPage
     {
-        String difficutlyLvl; 
+        String difficultyLvl;
         public AlarmCreate()
         {
             InitializeComponent();
@@ -24,30 +25,33 @@ namespace Math_Alarm
         //Code that is executed once the application bar save button is clicked.
         private void ApplicationBarSaveButton_Click(object sender, EventArgs e)
         {
-            String name = System.Guid.NewGuid().ToString();
-
+            String name = nameTextBox.Text;
             DateTime time = (DateTime)alarmTime.Value;
 
             Alarm alarm = new Alarm(name);
+            alarm.Content = nameTextBox.Text + "    Difficulty: " + getDifficulty();
             alarm.BeginTime = time;
 
             //Adds the alarm created by the user to the list box on the main page
             //and sets it to be an actual scheduled service by the OS.
             ScheduledActionService.Add(alarm);
+            
         }
 
+        //Sets difficultyLvl String to the current selected item in the difficultyPicker ListPicker.
         private void difficultyPicker_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            ListPickerItem dpi = difficultyPicker.SelectedItem as ListPickerItem;
-            difficutlyLvl = dpi.ToString();
+            difficultyLvl = difficultyPicker.SelectedItem.ToString();
         }
         
         //This method will return the level of difficulty that the user selects.
-        private String getDifficulty()
+        public String getDifficulty()
         {
-            return difficutlyLvl;
+            difficultyLvl = difficultyPicker.SelectedItem.ToString();
+            return difficultyLvl;
         }
 
+        //Returns the name of the alarm being created.
         public String getAlarmName()
         {
             return nameTextBox.Text.ToString();
